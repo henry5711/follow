d260e52a0bcc49a9b53e982d7136b8e7a701db84
henry <henryrom57@gmail.com>
2022-03-03 10:13:10 -0400

    full

---
+++ database/migrations/2022_03_01_121854_post.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Post extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->integer('user_id');
+            $table->string('user_name');
+            $table->string('contenido',7000);
+            $table->dateTime('fecha');
+            $table->enum('status',['Activa','Pausa','Oculta','Archivada']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}

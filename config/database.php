679044297a529bc8d06f3bf373b9200705605c48
Administrator <admin@example.com>
2023-02-02 15:49:27 +0000

    Update database.php

--- config/database.php
+++ config/database.php
@@ -83,6 +83,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE_BILLING', 'forge'),
             'database' => env('DB_DATABASE_USER', 'forge'),
             'username' => env('DB_USERNAME_READ', 'forge'),
             'password' => env('DB_PASSWORD_READ', ''),
